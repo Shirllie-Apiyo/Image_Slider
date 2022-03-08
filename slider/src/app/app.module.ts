e49120d154703c4f7fca81039d80092b44663d9b
Shirllie-Apiyo <apraise320@gmail.com>
2022-03-08 23:43:19 +0300

    slider api

--- slider/src/app/app.module.ts
+++ slider/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './slider/slider.component';
 import { NgImageSliderModule } from 'ng-image-slider';
-
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +14,8 @@ import { NgImageSliderModule } from 'ng-image-slider';
   imports: [
     BrowserModule,
     NgImageSliderModule ,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- slider/src/app/slider/slider.component.ts
+++ slider/src/app/slider/slider.component.ts
@@ -1,46 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
-  imgCollection: Array<object> =[
-    {
-      image:'/assets/img6.jpg',
-      'thumbImage': '/assets/img2.jpg',
-      alt :'Car',
-      title: 'Car'
-    },
-    {
-      image:'/assets/img7.jpg',
-      'thumbImage': '/assets/img7.jpg',
-      alt :'Car',
-      title: 'Car'
-    },
-    {
-      image:'/assets/merc.jpg',
-      'thumbImage': '/assets/merc.jpg',
-      alt :'Car',
-      title: 'Car'
-    },
-    {
-      image:'/assets/vin.jpg',
-      'thumbImage': '/assets/vin.jpg',
-      alt :'Car',
-      title: 'Car'
-    },
-    {
-      video:'https://youtu.be/JXVta1f8qeM',
-      alt :'Cars',
-      title: 'About Cars'
-    }
-  ]
-
-  constructor() { }
+  // imgCollection =   [
+  //   {
+  //     image:'/assets/img6.jpg',
+  //     'thumbImage': '/assets/img2.jpg',
+  //     alt :'Car',
+  //     title: 'Car'
+  //   },
+  //   {
+  //     image:'/assets/img7.jpg',
+  //     'thumbImage': '/assets/img7.jpg',
+  //     alt :'Car',
+  //     title: 'Car'
+  //   },
+  //   {
+  //     image:'/assets/merc.jpg',
+  //     'thumbImage': '/assets/merc.jpg',
+  //     alt :'Car',
+  //     title: 'Car'
+  //   },
+  //   {
+  //     image:'/assets/vin.jpg',
+  //     'thumbImage': '/assets/vin.jpg',
+  //     alt :'Car',
+  //     title: 'Car'
+  //   },
+  //   {
+  //     video:'https://youtu.be/JXVta1f8qeM',
+  //     alt :'Cars',
+  //     title: 'About Cars'
+  //   }
+  // ]
+  imgCollection: any;
+  constructor(    private httpClient:HttpClient) { }
 
   ngOnInit(): void {
+
+
+    this.httpClient.get("http://127.0.0.1:5000/api/products/getslider")
+    .subscribe((data) => this.displaydata(data))
+  }
+
+
+  httpdata: any
+  displaydata(data: any) {
+    this.imgCollection = data
+    console.log(data)
   }
 
 }
